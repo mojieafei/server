f98293dcedb2ded499d1d59804a737eda35fa79a
mojieafei <1054584499@qq.com>
2023-07-19 14:18:13 +0800

    增加proto

--- src/main/java/com/example/server/service/grpc/ChatServiceGrpcImpl.java
+++ src/main/java/com/example/server/service/grpc/ChatServiceGrpcImpl.java
@@ -1,19 +1,64 @@
 package com.example.server.service.grpc;
 
-import com.example.server.gen.proto.ChatServiceGrpc;
-import com.example.server.gen.proto.Code;
-import com.example.server.gen.proto.SendMessageReply;
-import com.example.server.gen.proto.SendMessageRequest;
+import com.alibaba.fastjson.JSON;
+import com.example.server.gen.proto.*;
+import com.example.server.utils.ConnectionUtils;
+import com.example.server.utils.HttpUtil;
+import io.grpc.internal.JsonUtil;
+import io.grpc.stub.CallStreamObserver;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.HashMap;
+import java.util.Objects;
+
+@GrpcService
 public class ChatServiceGrpcImpl extends ChatServiceGrpc.ChatServiceImplBase {
 
+    @Autowired
+    private ConnectionUtils connectionUtils;
 
 
 
-    public void sendMessageToSomeOne(SendMessageRequest request, StreamObserver<SendMessageReply> responseObserver) {
+    @Override
+    public void getConnection(ConnectionRequest request, StreamObserver<ConnectionReply> responseObserver) {
+        try {
+            ConnectionReply.Builder builder = ConnectionReply.newBuilder();
+            CallStreamObserver<ConnectionReply> response =  (CallStreamObserver)responseObserver;
+            response.onNext(builder.setFormUserId(request.getUserId()).setText(Boolean.TRUE.toString()).build());
+            connectionUtils.putConnection(request.getUserId(), response);
+            // 不释放链接
+            // responseObserver.onCompleted();
+        } catch (Exception e) {
+//            log.error("sendMessageToSomeone error", e);  记录日志
+            System.out.println("getConnection error ");
+            e.printStackTrace();
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void giveUpConnection(ConnectionRequest request, StreamObserver<GiveUpConnectionReply> responseObserver) {
+        try {
+
+            GiveUpConnectionReply.Builder result = GiveUpConnectionReply.newBuilder();
+            result.setResult(Boolean.TRUE);
+            StreamObserver<ConnectionReply> connection = connectionUtils.getConnection(request.getUserId());
+            connection.onCompleted();
+            responseObserver.onNext(result.build());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+//            log.error("sendMessageToSomeone error", e);  记录日志
+            System.out.println("giveUpConnection error ");
+            e.printStackTrace();
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void sendMessageToSomeone(SendMessageRequest request, StreamObserver<SendMessageReply> responseObserver) {
         try {
             SendMessageReply.Builder result = SendMessageReply.newBuilder();
 
@@ -25,12 +70,10 @@ public class ChatServiceGrpcImpl extends ChatServiceGrpc.ChatServiceImplBase {
                 result.setCode(checkParam).setResult(Boolean.FALSE);
             }
 
-
-
             responseObserver.onNext(result.build());
             responseObserver.onCompleted();
         } catch (Exception e) {
-//            log.error("addTicketToCasinosList error", e);  记录日志
+//            log.error("sendMessageToSomeone error", e);  记录日志
             System.out.println("sendMessageToSomeOne error ");
             e.printStackTrace();
             responseObserver.onError(e);
@@ -38,11 +81,29 @@ public class ChatServiceGrpcImpl extends ChatServiceGrpc.ChatServiceImplBase {
 
     }
 
+
+
     private boolean doSendMsg(SendMessageRequest request){
-        System.out.println("发送了消息");
+//        System.out.println("发送了消息");
+        // 推送消息
+        ConnectionReply.Builder builder = ConnectionReply.newBuilder();
+        CallStreamObserver<ConnectionReply> connection = connectionUtils.getConnection(request.getToUserId());
+        // 接收方离线或者链接已经失效
+        if(Objects.isNull(connection) || !connection.isReady()){
+            // 离线消息逻辑
+            System.out.println("处理离线逻辑");
+            return Boolean.FALSE;
+        }
+        connection.onNext(builder.setFormUserId(request.getFormUserId()).setText(request.getText()).build());
         return Boolean.TRUE;
     }
 
+
+//    HashMap<String, String> urlParam = new HashMap<>();
+//    HashMap<String, String> text = new HashMap<>();
+//        text.put("text","用户" + request.getFormUserId() + "向" + "用户" + request.getToUserId() + "成功发送了消息，消息为：" + request.getText());
+//        urlParam.put("body",JSON.toJSONString(text));
+//        HttpUtil.sendPost("https://www.feishu.cn/flow/api/trigger-webhook/e4d20740bd1f84f2657b45b5e1679837", urlParam, "UTF-8");
     private Code checkParam(SendMessageRequest request){
         return Code.SUCCESS;
     }
---
+++ src/main/java/com/example/server/utils/ConnectionUtils.java
@@ -0,0 +1,54 @@
+package com.example.server.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.example.server.gen.proto.ConnectionReply;
+import io.grpc.stub.CallStreamObserver;
+import io.grpc.stub.StreamObserver;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.stereotype.Component;
+import org.springframework.util.NumberUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 用户链接管理工具
+ */
+
+@Component
+public class ConnectionUtils {
+
+    /**
+     * 初版先放在内存里
+     *  key：用户id，value：
+     */
+
+    private ConcurrentHashMap<Long, CallStreamObserver<ConnectionReply>> connections = new ConcurrentHashMap<>();
+
+    public void putConnection(@Positive(message = "id非法")Long userId, @NotNull(message = "链接无效")CallStreamObserver<ConnectionReply> responseObserver){
+        connections.put(userId,responseObserver);
+        System.out.println(JSON.toJSONString(connections));
+    }
+
+    public CallStreamObserver<ConnectionReply>  getConnection(Long userId){
+        CallStreamObserver<ConnectionReply> connectionReplyStreamObserver = connections.get(userId);
+        if(Objects.isNull(connectionReplyStreamObserver)){
+            return null;
+        }
+        return connectionReplyStreamObserver;
+    }
+
+    public CallStreamObserver<ConnectionReply> getConnectionAndRemove(Long userId){
+        CallStreamObserver<ConnectionReply> connectionReplyStreamObserver = connections.get(userId);
+        connections.remove(userId);
+        if(Objects.isNull(connectionReplyStreamObserver)){
+            return null;
+        }
+        return connectionReplyStreamObserver;
+    }
+
+
+}
